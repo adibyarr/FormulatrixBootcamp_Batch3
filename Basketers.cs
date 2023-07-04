0c8939e394b971d6f8a54847a69aa62e2b3d6bf0
Adibya <adibyarizaldy16@gmail.com>
2023-07-04 21:13:23 +0700

    First Commit

---
+++ Basketers.cs
@@ -0,0 +1,31 @@
+namespace BasketballPlayer;
+public class Basketers
+
+{
+	private string _playerName;
+	private int _playerAge;
+	private string _club;
+	
+	public Basketers(string name, int age, string club)
+	
+	{
+		_playerName = name;
+		_playerAge = age;
+		_club = club;
+	}
+	public string GetName()
+	
+	{
+		return _playerName;
+	}
+	public int GetAge()
+	
+	{
+		return _playerAge;
+	}
+	public string GetClub()
+	
+	{
+		return _club;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace BasketballPlayer;
+
+class Program 
+
+{
+	static void Main()
+	
+	{
+		List<Basketers> Basketer = new List<Basketers>();
+		Basketer.Add(new Basketers("James", 32, "Lakers"));
+		Basketer.Add(new Basketers("Durant", 38,"Nets"));
+		
+		foreach(Basketers basketer in Basketer)
+		
+		{
+			string NamePlayer = basketer.GetName();
+			int AgePlayer = basketer.GetAge();
+			string ClubPlayer = basketer.GetClub();
+			Console.WriteLine($"Name:{NamePlayer}\n Age:{AgePlayer} \n Club:{ClubPlayer}\n");
+		}
+	}
+}
+
