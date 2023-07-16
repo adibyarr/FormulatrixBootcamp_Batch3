888f16039e4988c3b81c5fb7ac683ca0854110a9
Adibya <adibyarizaldy16@gmail.com>
2023-07-16 22:25:39 +0700

    Task Foobar

---
+++ TugasBaru/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TaskFoobar
+{
+	class program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<int, string> rules = new Dictionary<int, string>();
+			rules.Add(3, "foo");
+			rules.Add(5, "bar");
+			
+			Console.Write("Input number: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine("n = " + n);
+			Console.Write("Result: ");
+			
+			FoobarPrinter printer = new FoobarPrinter();
+			printer.PrintNumbers(n, rules);
+			
+			Console.WriteLine();
+		}
+	}
+}
