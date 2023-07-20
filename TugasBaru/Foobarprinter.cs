3ba4835895e5049dfd95c329363761cfb346bf12
adibyarr <adibyarizaldy16@gmail.com>
2023-07-20 11:58:21 +0700

    Commit Tugas

--- TugasBaru/Foobarprinter.cs
+++ TugasBaru/Foobarprinter.cs
@@ -1,32 +1,43 @@
 using System;
 using System.Collections.Generic;
+using System.Security;
 
 namespace TaskFoobar
 {
-    class FoobarPrinter
-    {
-        public void PrintNumbers(int n, Dictionary<int, string> rules)
-        {
-            for (int i = 0; i <= n; i++) 
-            {
-                bool isDivided = false;
-                string result = " ";
+	class FoobarPrinter
+	{
+		private Dictionary<int, string> rules = new Dictionary<int, string>();
+		public FoobarPrinter()
+		{
+			rules.Add(3, "foo");
+			rules.Add(5, "bar");
+		}
+		public void AddRule(int divisor, string output)
+		{
+			rules[divisor] = output; 
+		}
+		public void PrintNumbers(int n)
+		{
+			for (int i = 0; i <= n; i++) 
+			{
+				bool isDivided = false;
+				string result = " ";
 
-                foreach (var rule in rules)
-                {
-                    if (i != 0 && i % rule.Key == 0)
-                    {
-                        result += rule.Value;
-                        isDivided = true;
-                    }
-                }
+				foreach (var rule in rules)
+				{
+					if (i != 0 && i % rule.Key == 0)
+					{
+						result += rule.Value;
+						isDivided = true;
+					}
+				}
 
-                if (!isDivided)
-                    result += i.ToString();
+				if (!isDivided)
+					result += i.ToString();
 
-                Console.Write(result + " ");
-            }
-        }
-    }
+				Console.Write(result + " ");
+			}
+		}
+	}
 
 }
--- TugasBaru/Program.cs
+++ TugasBaru/Program.cs
@@ -7,16 +7,13 @@ namespace TaskFoobar
 	{
 		static void Main(string[] args)
 		{
-			Dictionary<int, string> rules = new Dictionary<int, string>();
-			rules.Add(3, "foo");
-			rules.Add(5, "bar");
-			
-
 			Console.Write("Input number: ");
 			int n = Convert.ToInt32(Console.ReadLine());
 
 			FoobarPrinter printer = new FoobarPrinter();
-			printer.PrintNumbers(n, rules);
+			printer.AddRule(3, "foo");
+			printer.AddRule(5, "bar");
+			printer.PrintNumbers(n);
 		}
 	}
 
