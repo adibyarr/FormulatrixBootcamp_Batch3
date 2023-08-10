6d6457e9615f9ee8d94c7e3e3500d8108e5e6260
adibyarr <adibyarizaldy16@gmail.com>
2023-08-10 12:16:47 +0700

    Commit Day25

--- Week1/day_2/Basketers.cs
+++ Week1/day_2/Basketers.cs
@@ -1,4 +1,4 @@
-namespace BasketballPlayer;
+Nnamespace BasketballPlayer;
 public class Basketers
 
 {
--- Week4/SerializePoker/PokerHand.cs
+++ Week4/SerializePoker/PokerHand.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using PokerHandEvaluator;
 namespace SerializePoker;
-public class PokerHandEvaluator{
+public class PokerHandEvaluator
+{
 		public Hand EvaluateHand(List<Card> allCards)
 		{
 			List<PokerHand> pokerHands = new List<PokerHand>
